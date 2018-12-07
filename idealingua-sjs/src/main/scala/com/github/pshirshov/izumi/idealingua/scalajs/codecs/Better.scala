ce8d09a1f2418e47d93800c44e5e64e8b4303575
Pavel Shirshov <pshirshov@gmail.com>
2018-12-07 23:22:17 +0000

    wip

--- idealingua-sjs/src/main/scala/com/github/pshirshov/izumi/idealingua/scalajs/codecs/Better.scala
+++ idealingua-sjs/src/main/scala/com/github/pshirshov/izumi/idealingua/scalajs/codecs/Better.scala
@@ -6,10 +6,27 @@ import upickle.core.Visitor
 object Better extends upickle.AttributeTagged {
 
   override def taggedWrite[T, R](w: CaseW[T], tag: String, out: Visitor[_, R], v: T): R = {
-    val s = super.taggedWrite[T, R](w, tag, out, v)
+    val s =
     if (v.isInstanceOf[IDLError]) {
       println((v, v.getClass))
+
+      val ctx = out.asInstanceOf[Visitor[Any, R]].visitObject(w.length(v) + 1, -1)
+      val keyVisitor = ctx.visitKey(-1)
+
+      ctx.visitKeyValue(keyVisitor.visitString(tagName, -1))
+      ctx.visitValue(out.visitString(objectTypeKeyWriteMap(tag), -1), -1)
+
+      ctx.visitKeyValue("MESSAGE")
+      ctx.visitKeyValue("REPR")
+
+      w.writeToObject(ctx, v)
+      val res = ctx.visitEnd(-1)
+      res
+
+    } else {
+      super.taggedWrite[T, R](w, tag, out, v)
+
     }
-    s
+
   }
 }
